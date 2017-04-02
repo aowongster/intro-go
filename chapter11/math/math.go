77796f73994606512d69212b83197c0bd473d838
Alistair Wong <awong@webroot.com>
2017-04-02 13:34:02 -0700

    tests for math

--- chapter11/math/math.go
+++ chapter11/math/math.go
@@ -2,6 +2,10 @@ package math
 
 // Finds the average of a series of numbers
 func Average(xs []float64) float64 {
+  if len(xs) == 0 {
+    return 0
+  }
+
   total := float64(0)
   for _, x := range xs {
     total += x
@@ -11,6 +15,10 @@ func Average(xs []float64) float64 {
 
 // Finds the minimum of a series of numbers
 func Min(xs []float64) float64 {
+  if len(xs) == 0 {
+    return 0
+  }
+
   min := xs[0] // pick arbitrary value from select
   for _, x := range xs {
     if x < min {
@@ -22,6 +30,10 @@ func Min(xs []float64) float64 {
 
 // Finds the maximum value of a series of numbers
 func Max(xs []float64) float64 {
+  if len(xs) == 0 {
+    return 0
+  }
+  
   max := xs[0]
   for _, x := range xs {
     if x > max {
--- chapter11/math/math_test.go
+++ chapter11/math/math_test.go
@@ -2,10 +2,75 @@ package math
 
 import "testing"
 
+type testpair struct {
+  values []float64
+  res float64
+}
+
+var tests = []testpair {
+  { []float64{1,2}, 1.5},
+  { []float64{1,1,1,1,1}, 1},
+  { []float64{-1, 1}, 0},
+  { []float64{}, 0},
+}
+
+// func TestAverage(t *testing.T) {
+//   var v float64;
+//   v = Average([]float64{1,2})
+//   if v != 1.5 {
+//     t.Error("Expected 1.5, got ", v)
+//   }
+// }
+
 func TestAverage(t *testing.T) {
-  var v float64;
-  v = Average([]float64{1,2})
-  if v != 1.5 {
-    t.Error("Expected 1.5, got ", v)
+  for _, pair := range tests {
+    v := Average(pair.values)
+    if v != pair.res {
+      t.Error(
+        "For", pair.values,
+        "expected", pair.res,
+        "got", v,
+      )
+    }
+  }
+}
+
+var minTests = []testpair {
+  { []float64{1,2}, 1},
+  { []float64{1,1,1,1,1}, 1},
+  { []float64{-1, 1}, -1},
+  { []float64{}, 0},
+}
+
+func TestMin(t *testing.T) {
+  for _, pair := range minTests {
+    v := Min(pair.values)
+    if v != pair.res {
+      t.Error(
+        "For", pair.values,
+        "expected", pair.res,
+        "got", v,
+      )
+    }
+  }
+}
+
+var maxTests = []testpair {
+  { []float64{1,2}, 2},
+  { []float64{1,1,1,1,1}, 1},
+  { []float64{-1, 1}, 1},
+  { []float64{}, 0},
+}
+
+func TestMax(t *testing.T) {
+  for _, pair := range maxTests {
+    v := Max(pair.values)
+    if v != pair.res {
+      t.Error(
+        "For", pair.values,
+        "expected", pair.res,
+        "got", v,
+      )
+    }
   }
 }
